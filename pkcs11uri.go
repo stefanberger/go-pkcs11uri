06404b8a374482c4832f0f8c7da82442a6fbd416
Stefan Berger <stefanb@linux.vnet.ibm.com>
2020-07-27 14:40:57 -0400

    Initial revision
    Initial revision of pkcs11 client library
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ pkcs11uri.go
@@ -0,0 +1,269 @@
+package pkcs11uri
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/url"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// Pkcs11URI holds a pkcs11 URI object
+type Pkcs11URI struct {
+	// path and query attributes may have custom attributes that either
+	// have to be in the query or in the path part, so we use two maps
+	pathAttributes  map[string]string
+	queryAttributes map[string]string
+}
+
+// upper character hex digits needed for pct-encoding
+const hex = "0123456789ABCDEF"
+
+// escapeAll pct-escapes all characters in the string
+func escapeAll(s string) string {
+	res := make([]byte, len(s)*3)
+	j := 0
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		res[j] = '%'
+		res[j+1] = hex[c>>4]
+		res[j+2] = hex[c&0xf]
+		j += 3
+	}
+	return string(res)
+}
+
+// escape pct-escapes the path and query part of the pkcs11 URI following the different rules of the
+// path and query part as decribed in RFC 7512 sec. 2.3
+func escape(s string, isPath bool) string {
+	res := make([]byte, len(s)*3)
+	j := 0
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		// unreserved per RFC 3986 sec. 2.3
+		if (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') {
+			res[j] = c
+		} else if isPath && c == '&' {
+			res[j] = c
+		} else if !isPath && (c == '/' || c == '?' || c == '|') {
+			res[j] = c
+		} else {
+			switch c {
+			case '-', '.', '_', '~': // unreserved per RFC 3986 sec. 2.3
+				res[j] = c
+			case ':', '[', ']', '@', '!', '$', '`', '(', ')', '*', '+', ',', '=':
+				res[j] = c
+			default:
+				res[j] = '%'
+				res[j+1] = hex[c>>4]
+				res[j+2] = hex[c&0xf]
+				j += 2
+			}
+		}
+		j++
+	}
+	return string(res[:j])
+}
+
+// New creates a new Pkcs11URI object
+func New() (Pkcs11URI, error) {
+	return Pkcs11URI{
+		pathAttributes:  make(map[string]string),
+		queryAttributes: make(map[string]string),
+	}, nil
+}
+
+func (uri *Pkcs11URI) addAttribute(attrMap map[string]string, name, value string) error {
+	v, err := url.QueryUnescape(value)
+	if err != nil {
+		return err
+	}
+	attrMap[name] = v
+	return nil
+}
+
+// GetPathAttribute returns the value of a path attribute
+func (uri *Pkcs11URI) GetPathAttribute(name string) (string, bool) {
+	v, ok := uri.pathAttributes[name]
+	return v, ok
+}
+
+// AddPathAttribute adds a path attribute
+func (uri *Pkcs11URI) AddPathAttribute(name, value string) error {
+	if _, ok := uri.pathAttributes[name]; ok {
+		return errors.New("duplicate path attribute")
+	}
+	return uri.addAttribute(uri.pathAttributes, name, value)
+}
+
+// RemovePathAttribute removes a path attribute
+func (uri *Pkcs11URI) RemovePathAttribute(name string) {
+	delete(uri.pathAttributes, name)
+}
+
+// GetQueryAttribute returns the value of a query attribute
+func (uri *Pkcs11URI) GetQueryAttribute(name string) (string, bool) {
+	v, ok := uri.queryAttributes[name]
+	return v, ok
+}
+
+// AddQueryAttribute adds a query attribute
+func (uri *Pkcs11URI) AddQueryAttribute(name, value string) error {
+	if _, ok := uri.queryAttributes[name]; ok {
+		return errors.New("duplicate query attribute")
+	}
+	return uri.addAttribute(uri.queryAttributes, name, value)
+}
+
+// RemoveQueryAttribute removes a path attribute
+func (uri *Pkcs11URI) RemoveQueryAttribute(name string) {
+	delete(uri.queryAttributes, name)
+}
+
+// Validate validates a Pkcs11URI object's attributes following RFC 7512 rules and proper formatting of
+// their values
+func (uri *Pkcs11URI) Validate() error {
+	/* RFC 7512: 2.3 */
+	/* slot-id should be DIGIT, but we go for number */
+	if v, ok := uri.pathAttributes["slot-id"]; ok {
+		if _, err := strconv.Atoi(v); err != nil {
+			return fmt.Errorf("slot-id must be a number: %s", v)
+		}
+	}
+
+	/* library-version should 1*DIGIT [ "." 1 *DIGIT ]; allow NUMBERS for DIGIT */
+	if v, ok := uri.pathAttributes["library-version"]; ok {
+		m, err := regexp.Match("^[0-9]+(\\.[0-9]+)?$", []byte(v))
+		if err != nil || !m {
+			return fmt.Errorf("Invalid format for library-version '%s'", v)
+		}
+	}
+
+	if v, ok := uri.pathAttributes["type"]; ok {
+		m, err := regexp.Match("^(public|private|cert|secret-key}data)?$", []byte(v))
+		if err != nil || !m {
+			return fmt.Errorf("Invalid type '%s'", v)
+		}
+	}
+
+	/* RFC 7512: 2.4 */
+	_, ok1 := uri.queryAttributes["pin-source"]
+	_, ok2 := uri.queryAttributes["pin-value"]
+	if ok1 && ok2 {
+		return errors.New("URI must not contain pin-source and pin-value")
+	}
+
+	if v, ok := uri.queryAttributes["module-path"]; ok {
+		if !filepath.IsAbs(v) {
+			return fmt.Errorf("path %s of module-name attribute must be absolute", v)
+		}
+	}
+
+	return nil
+}
+
+// GetPIN gets the PIN from either the pin-value or pin-source attribute
+func (uri *Pkcs11URI) GetPIN() (string, error) {
+	if v, ok := uri.queryAttributes["pin-value"]; ok {
+		return v, nil
+	}
+	if v, ok := uri.queryAttributes["pin-source"]; ok {
+		pinuri, err := url.ParseRequestURI(v)
+		if err != nil {
+			return "", fmt.Errorf("Could not parse pin-source: %s ", err)
+		}
+		switch pinuri.Scheme {
+		case "", "file":
+			if !filepath.IsAbs(pinuri.Path) {
+				return "", fmt.Errorf("PIN URI path '%s' is not absolute", pinuri.Path)
+			}
+			pin, err := ioutil.ReadFile(pinuri.Path)
+			if err != nil {
+				return "", fmt.Errorf("Could not open PIN file: %s", err)
+			}
+			return string(pin), nil
+		default:
+			return "", fmt.Errorf("PIN URI scheme %s is not supported", pinuri.Scheme)
+		}
+	}
+	return "", fmt.Errorf("Neither pin-source nor pin-value are available")
+}
+
+// Parse parses a pkcs11: URI string
+func (uri *Pkcs11URI) Parse(uristring string) error {
+	if !strings.HasPrefix(uristring, "pkcs11:") {
+		return errors.New("Malformed pkcs11 URI: missing pcks11: prefix")
+	}
+	parts := strings.Split(uristring[7:], "?")
+	if len(parts) > 2 {
+		return errors.New("Malformed pkcs11 URI: too many query parts")
+	}
+
+	uri.pathAttributes = make(map[string]string)
+	uri.queryAttributes = make(map[string]string)
+
+	if len(parts[0]) > 0 {
+		/* parse path part */
+		for _, part := range strings.Split(parts[0], ";") {
+			p := strings.Split(part, "=")
+			if len(p) != 2 {
+				return errors.New("Malformed pkcs11 URI: malformed path attribute")
+			}
+			if err := uri.AddPathAttribute(p[0], p[1]); err != nil {
+				return fmt.Errorf("Malformed pkcs11 URI: %s", err)
+			}
+		}
+	}
+
+	if len(parts) == 2 {
+		/* parse query part */
+		for _, part := range strings.Split(parts[1], "&") {
+			p := strings.Split(part, "=")
+			if len(p) != 2 {
+				return errors.New("Malformed pkcs11 URI: malformed query attribute")
+			}
+			if err := uri.AddQueryAttribute(p[0], p[1]); err != nil {
+				return fmt.Errorf("Malformed pkcs11 URI: %s", err)
+			}
+		}
+	}
+	return uri.Validate()
+}
+
+// formatAttribute formats attributes and escapes their values as needed
+func formatAttributes(attrMap map[string]string, ispath bool) string {
+	res := ""
+	for key, value := range attrMap {
+		switch key {
+		case "id":
+			/* id is always pct-encoded */
+			value = escapeAll(value)
+		default:
+			if ispath {
+				value = escape(value, true)
+			} else {
+				value = escape(value, false)
+			}
+		}
+		if len(res) > 0 {
+			res += ";"
+		}
+		res += key + "=" + value
+	}
+	return res
+}
+
+// Format formats a Pkcs11URI to it string representaion
+func (uri *Pkcs11URI) Format() (string, error) {
+	if err := uri.Validate(); err != nil {
+		return "", err
+	}
+	result := "pkcs11:" + formatAttributes(uri.pathAttributes, true)
+	if len(uri.queryAttributes) > 0 {
+		result += "?" + formatAttributes(uri.queryAttributes, false)
+	}
+	return result, nil
+}
---
+++ pkcs11uri_test.go
@@ -0,0 +1,214 @@
+package pkcs11uri
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestParse1(t *testing.T) {
+	uri, err := New()
+	if err != nil {
+		t.Fatalf("Could not create a Pkcs11URI object")
+	}
+
+	original := "pkcs11:id=%02;object=SIGN%20pubkey;token=SSH%20key;manufacturer=piv_II?module-path=/usr/lib64/pkcs11/opensc-pkcs11.so"
+	err = uri.Parse(original)
+
+	if err != nil {
+		t.Fatalf("Could not parse URI: %s", err)
+	}
+
+	for _, attr := range []string{"id", "object", "token", "manufacturer"} {
+		if _, ok := uri.GetPathAttribute(attr); !ok {
+			t.Fatalf("Path attribute %s is not available", attr)
+		}
+	}
+	for _, attr := range []string{"module-path"} {
+		if _, ok := uri.GetQueryAttribute(attr); !ok {
+			t.Fatalf("Query attribute %s is not available", attr)
+		}
+	}
+
+	_, err = uri.Format()
+	if err != nil {
+		t.Fatalf("Could not format the uri: %s", err)
+	}
+}
+
+func verifyURI(t *testing.T, uri Pkcs11URI, expecteduri string) {
+	encoded, err := uri.Format()
+	if err != nil {
+		t.Fatalf("Could not format the uri: %s", err)
+	}
+	if encoded != expecteduri {
+		t.Fatalf("Did not get expected URI '%s' but '%s'", expecteduri, encoded)
+	}
+}
+
+func verifyPIN(t *testing.T, uri Pkcs11URI, expectedpin string) {
+	pin, err := uri.GetPIN()
+	if err != nil {
+		t.Fatalf("Could not get PIN: %s", err)
+	}
+	if pin != expectedpin {
+		t.Fatalf("Did not get expected PIN value of '1234' but '%s'", pin)
+	}
+}
+
+func TestConstruct1(t *testing.T) {
+	uri, err := New()
+	if err != nil {
+		t.Fatalf("Could not create a Pkcs11URI object")
+	}
+	expecteduri := "pkcs11:id=%66%6F%6F"
+	uri.AddPathAttribute("id", "%66oo")
+	verifyURI(t, uri, expecteduri)
+
+	expectedpin := "1234"
+	expecteduri += fmt.Sprintf("?pin-value=%s", expectedpin)
+	uri.AddQueryAttribute("pin-value", expectedpin)
+
+	verifyURI(t, uri, expecteduri)
+	verifyPIN(t, uri, expectedpin)
+}
+
+func writePinfile(t *testing.T, value string) *os.File {
+	tmpfile, err := ioutil.TempFile("", "mypin")
+	if err != nil {
+		t.Fatalf("Coult not create temporary file: %s", err)
+	}
+	if _, err := tmpfile.Write([]byte(value)); err != nil {
+		t.Fatalf("Could not write to tempfile: %s", err)
+	}
+	if err := tmpfile.Close(); err != nil {
+		t.Fatalf("Could not close tempfile: %s", err)
+	}
+	return tmpfile
+}
+
+func TestPinSource(t *testing.T) {
+	uri, err := New()
+	if err != nil {
+		t.Fatalf("Could not create a Pkcs11URI object")
+		return
+	}
+
+	expectedpin := "4321"
+
+	tmpfile := writePinfile(t, expectedpin)
+	defer os.Remove(tmpfile.Name())
+
+	expecteduri := "pkcs11:id=%66%6F%6F?pin-source=file:" + tmpfile.Name()
+	uri.AddPathAttribute("id", "foo")
+	uri.AddQueryAttribute("pin-source", "file:"+tmpfile.Name())
+
+	verifyURI(t, uri, expecteduri)
+	verifyPIN(t, uri, expectedpin)
+
+	expecteduri = "pkcs11:id=%66%6F%6F?pin-source=file:" + tmpfile.Name()
+	uri.AddPathAttribute("id", "foo")
+	uri.AddQueryAttribute("pin-source", tmpfile.Name())
+
+	verifyURI(t, uri, expecteduri)
+	verifyPIN(t, uri, expectedpin)
+}
+
+func TestBadInput(t *testing.T) {
+	uri, err := New()
+	if err != nil {
+		t.Fatalf("Could not create a Pkcs11URI object")
+		return
+	}
+
+	for _, entry := range [][]string{{"slot-id", "foo"}, {"library-version", "foo"}, {"library-version", "1.bar"}, {"type", "fobbar"}} {
+		uri.AddPathAttribute(entry[0], entry[1])
+		if err := uri.Validate(); err == nil {
+			t.Fatalf("uri validation should have failed due to malformed %s value '%s'", entry[0], entry[1])
+		}
+		uri.RemovePathAttribute(entry[0])
+	}
+}
+
+func TestGoodInput(t *testing.T) {
+	uri, err := New()
+	if err != nil {
+		t.Fatalf("Could not create a Pkcs11URI object")
+		return
+	}
+
+	for _, entry := range [][]string{{"slot-id", "1"}, {"library-version", "7"}, {"library-version", "1.8"}, {"type", "public"}} {
+		uri.AddPathAttribute(entry[0], entry[1])
+		if err := uri.Validate(); err != nil {
+			t.Fatalf("uri validation should have succeeded for %s value '%s': %s", entry[0], entry[1], err)
+		}
+	}
+}
+
+func TestURIs(t *testing.T) {
+	uri, err := New()
+	if err != nil {
+		t.Fatalf("Could not create a Pkcs11URI object")
+		return
+	}
+	uris := []string{
+		"pkcs11:",
+		"pkcs11:object=my-pubkey;type=public",
+		"pkcs11:object=my-key;type=private?pin-source=file:/etc/token",
+		"pkcs11:token=The%20Software%20PKCS%2311%20Softtoken;manufacturer=Snake%20Oil,%20Inc.;model=1.0;object=my-certificate;type=cert;id=%69%95%3E%5C%F4%BD%EC%91;serial=?pin-source=file:/etc/token_pin",
+		"pkcs11:object=my-sign-key;type=private?module-name=mypkcs11",
+		"pkcs11:object=my-sign-key;type=private?module-path=/mnt/libmypkcs11.so.1",
+		"pkcs11:token=Software%20PKCS%2311%20softtoken;manufacturer=Snake%20Oil,%20Inc.?pin-value=the-pin",
+		"pkcs11:slot-description=Sun%20Metaslot",
+		"pkcs11:library-manufacturer=Snake%20Oil,%20Inc.;library-description=Soft%20Token%20Library;library-version=1.23",
+		"pkcs11:token=My%20token%25%20created%20by%20Joe;library-version=3;id=%01%02%03%Ba%dd%Ca%fe%04%05%06",
+		"pkcs11:token=A%20name%20with%20a%20substring%20%25%3B;object=my-certificate;type=cert",
+		"pkcs11:token=Name%20with%20a%20small%20A%20with%20acute:%20%C3%A1;object=my-certificate;type=cert",
+		"pkcs11:token=my-token;object=my-certificate;type=cert;vendor-aaa=value-a?pin-source=file:/etc/token_pin&vendor-bbb=value-b",
+	}
+	for _, uristring := range uris {
+		err = uri.Parse(uristring)
+		if err != nil {
+			t.Fatalf("Could not parse URI '%s': %s", uristring, err)
+		}
+
+		encoded, err := uri.Format()
+		if err != nil {
+			t.Fatalf("Could not format URI '%s': %s", uristring, err)
+		}
+		// the order of attributes may be different but the string lengths are the same
+		if len(encoded) != len(uristring) {
+			t.Fatalf("String lengths are different: '%s' vs. '%s'", encoded, uristring)
+		}
+	}
+}
+
+func TestValidateEscapedAttrs(t *testing.T) {
+	input := [][]string{
+		{
+			// pkcs11 URI
+			"pkcs11:token=Software%20PKCS%2311%20softtoken;manufacturer=Snake%20Oil,%20Inc.?pin-value=the-pin",
+			// attribute name and value to check
+			"token", "Software PKCS#11 softtoken",
+		}, {
+			"pkcs11:token=My%20token%25%20created%20by%20Joe;library-version=3;id=%01%02%03%Ba%dd%Ca%fe%04%05%06",
+			"token", "My token% created by Joe",
+		},
+	}
+
+	uri, err := New()
+	if err != nil {
+		t.Fatalf("Could not create a Pkcs11URI object")
+	}
+	for _, data := range input {
+		err = uri.Parse(data[0])
+		if err != nil {
+			t.Fatalf("Could not parse URI '%s': %s", data[0], err)
+		}
+		v, _ := uri.GetPathAttribute(data[1])
+		if v != data[2] {
+			t.Fatalf("Got unexpected attribute value '%s'; expected '%s'", v, data[2])
+		}
+	}
+}
